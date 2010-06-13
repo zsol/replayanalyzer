276f4f0c108dd76b82e8881c060664dd92a7b682
Zsolt Dollenstein <zsol.zsol@gmail.com>
2010-06-13 12:47:43 +0200

    make detailsparser more robust

--- tools/detailsparser.cpp
+++ tools/detailsparser.cpp
@@ -15,27 +15,71 @@
 #include <boost/spirit/include/phoenix.hpp>
 #include <boost/spirit/include/support_istream_iterator.hpp>
 
+#include <boost/program_options.hpp>
+
 #include <mpq.h>
 #include <player.h>
 using namespace boost::spirit::qi;
 using namespace boost::spirit;
 using namespace boost::phoenix;
 namespace ios = boost::iostreams;
+namespace po = boost::program_options;
 
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+
+    po::options_description desc("Allowed options");
+    po::positional_options_description posdesc;
+    po::variables_map vm;
+
+    desc.add_options()
+        ("noextract", "input file is a replay.details file, not an MPQ archive")
+        ("input-file", po::value<std::string>(), "input file")
+        ;
+    posdesc.add("input-file", -1);
+
+    po::store(po::command_line_parser(argc, argv).
+              options(desc).positional(posdesc).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("input-file") != 1)
     {
-        std::cerr << "detailsparser <replay.details>" << std::endl;
+        std::cerr << "You have to specify an input file!" << std::endl;
         return 1;
     }
 
-    sc2replay::MPQArchive mpq;
-    mpq.load(argv[1]);
-    sc2replay::MPQFile* f = mpq.getFile("replay.details");
+    const unsigned char* begin, *end;
 
-    sc2replay::MPQFile::array_stream* s = f->getFileStream();
+    if (vm.count("noextract"))
+    {
+        const int BUFSIZE = 8*1024;
+        std::ifstream file(vm["input-file"].as<std::string>().c_str());
+        char* buf = new char[BUFSIZE]; // Yes, this will leak, no biggie.
+        file.read(buf, BUFSIZE);
+
+        if (file)
+        {
+            std::cerr << "Replay.details file too large, try to use the original replay file or contact the author!" << std::endl;
+            return 1;
+        }
+
+        begin = reinterpret_cast<unsigned char*>(buf);
+        end = begin + file.gcount();
+        file.close();
+    }
+    else
+    {
+        sc2replay::MPQArchive mpq;
+        mpq.load(vm["input-file"].as<std::string>().c_str());
+        sc2replay::MPQFile* f = mpq.getFile("replay.details");
+        
+        sc2replay::MPQFile::array_stream* s = f->getFileStream();
+        
+        begin = f->getFileContent();
+        end = begin + f->getFileSize() / sizeof(unsigned char*);
+    }
+
 
     rule<const unsigned char*, locals<int>, std::string() > str;
     rule<const unsigned char*, int()> v;
@@ -54,9 +98,7 @@ int main(int argc, char** argv)
         str >> omit[byte_(0x4) >> byte_(0x2)] >> 
         str >> omit[byte_(0x6) >> byte_(0x5) >> byte_(0x8)] >>
         omit[repeat(9)[kv]];
-    
-    const unsigned char* begin = f->getFileContent();
-    const unsigned char* end = begin + f->getFileSize() / sizeof(unsigned char*);
+
 
     sc2replay::Player p;
 
